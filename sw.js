7efce5df2a916aff8498a312e48fb52aee08e1c7
pramodpk89 <pramodpk89@gmail.com>
2026-03-08 12:08:48 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE_NAME = '75-day-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE='challenge75-v6';
+const URLS=['index.html','manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
